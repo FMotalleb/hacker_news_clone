f943492abcfec14e6859408d9d58e0cf72028c82
Motalleb Fallahnehzad <fmotalleb@gmail.com>
2023-07-26 08:13:40 +0330

    [Minor]

--- lib/features/feed_selector/widgets/selector_view.dart
+++ lib/features/feed_selector/widgets/selector_view.dart
@@ -7,7 +7,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hacker_news_clone/core/repository/hacker_news_repository.dart';
 import 'package:hacker_news_clone/features/feed_selector/cubit/feed_selector_cubit.dart';
-import 'package:hacker_news_clone/features/feed_selector/cubit/feed_selector_cubit.dart';
 import 'package:hemend_logger/hemend_logger.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:rxdart/rxdart.dart';
@@ -72,31 +71,10 @@ class SelectorSegment extends StatelessWidget with LogableObject {
   }
 
   Widget getActiveIconOf(BuildContext context, HNFeedType feed) {
-    final view = switch (feed) {
-      HNFeedType.bestStories => const Icon(
-          Icons.star_rate_rounded,
-          shadows: [
-            BoxShadow(
-              offset: Offset(1, 3),
-            ),
-          ],
-        ),
-      HNFeedType.newStories => const Icon(
-          Icons.newspaper_rounded,
-          shadows: [
-            BoxShadow(
-              offset: Offset(1, 3),
-            ),
-          ],
-        ),
-      HNFeedType.topStories => const Icon(
-          Icons.thumb_up_off_alt_rounded,
-          shadows: [
-            BoxShadow(
-              offset: Offset(1, 3),
-            ),
-          ],
-        ),
+    final icon = switch (feed) {
+      HNFeedType.bestStories => Icons.star_rate_rounded,
+      HNFeedType.newStories => Icons.newspaper_rounded,
+      HNFeedType.topStories => Icons.thumb_up_off_alt_rounded,
     };
     return GestureDetector(
       onLongPress: () {
@@ -104,79 +82,89 @@ class SelectorSegment extends StatelessWidget with LogableObject {
         info('long press on active feed: request to load log files');
         final logger = Logger('LogsModal');
         unawaited(
-          showLogModal(context, logger),
+          _showLogsModal(context, logger),
         );
       },
-      child: view,
+      child: Icon(
+        icon,
+        shadows: const [
+          BoxShadow(
+            offset: Offset(1, 3),
+          ),
+        ],
+      ),
     );
-  }
-
-  Future<void> showLogModal(BuildContext context, Logger logger) async {
-    final storageAddress = await getApplicationDocumentsDirectory();
-    final logs = await storageAddress
-        .list()
-        .where(
-          (event) => event.path.endsWith('.log'),
-        )
-        .whereType<File>()
-        .toList();
-    if (context.mounted) {
-      await showCupertinoModalPopup<void>(
-        context: context,
-        builder: (context) => CupertinoActionSheet(
-          actions: [
-            ...logs.map(
-              (e) => CupertinoActionSheetAction(
-                child: Text(e.uri.pathSegments.last),
-                onPressed: () async {
-                  logger.info(
-                    'selected file ${e.uri.pathSegments.last} so loading ${e.path}',
-                  );
-                  final content = await e.readAsString();
-                  if (context.mounted) {
-                    await showCupertinoDialog<void>(
-                      context: context,
-                      builder: (context) => CupertinoAlertDialog(
-                        title: Text(e.uri.pathSegments.last),
-                        content: Text(
-                          content,
-                          textAlign: TextAlign.start,
-                          textDirection: TextDirection.ltr,
-                        ),
-                        actions: [
-                          CupertinoActionSheetAction(
-                            onPressed: () async {
-                              await e.delete();
-                              if (context.mounted) {
-                                Navigator.of(context).pop();
-                              }
-                              logger.warning('Log ${e.uri.pathSegments.last} is deleted');
-                            },
-                            isDestructiveAction: true,
-                            child: const Text('Delete'),
-                          ),
-                          CupertinoActionSheetAction(
-                            onPressed: () {
-                              if (context.mounted) {
-                                Navigator.of(context).pop();
-                              }
-                            },
-                            // isDestructiveAction: true,
-                            child: const Text('close'),
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                },
-              ),
-            ),
-          ],
-        ),
-      );
-    }
   }
 
   @override
   String get loggerName => 'NavBar';
+}
+
+Future<void> _showLogsModal(BuildContext context, Logger logger) async {
+  final storageAddress = await getApplicationDocumentsDirectory();
+  final logs = await storageAddress
+      .list()
+      .where(
+        (event) => event.path.endsWith('.log'),
+      )
+      .whereType<File>()
+      .toList();
+  if (context.mounted) {
+    await showCupertinoModalPopup<void>(
+      context: context,
+      builder: (context) => CupertinoActionSheet(
+        actions: [
+          ...logs.map(
+            (e) => CupertinoActionSheetAction(
+              child: Text(e.uri.pathSegments.last),
+              onPressed: () async {
+                logger.info(
+                  'selected file ${e.uri.pathSegments.last} so loading ${e.path}',
+                );
+                final content = await e.readAsString();
+                if (context.mounted) {
+                  await showCupertinoDialog<void>(
+                    context: context,
+                    builder: (context) => CupertinoAlertDialog(
+                      title: Text(e.uri.pathSegments.last),
+                      content: Text(
+                        content,
+                        textAlign: TextAlign.start,
+                        textDirection: TextDirection.ltr,
+                      ),
+                      actions: [
+                        CupertinoActionSheetAction(
+                          onPressed: () async {
+                            await e.delete();
+                            if (context.mounted) {
+                              Navigator.of(context).pop();
+                            }
+                            logger.warning('Log ${e.uri.pathSegments.last} is deleted');
+                          },
+                          isDestructiveAction: true,
+                          child: const Text('Delete'),
+                        ),
+                        CupertinoActionSheetAction(
+                          onPressed: () {
+                            if (context.mounted) {
+                              Navigator.of(context).pop();
+                            }
+                          },
+                          // isDestructiveAction: true,
+                          child: const Text('close'),
+                        ),
+                      ],
+                    ),
+                  );
+                  if (context.mounted) {
+                    Navigator.of(context).pop();
+                  }
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
